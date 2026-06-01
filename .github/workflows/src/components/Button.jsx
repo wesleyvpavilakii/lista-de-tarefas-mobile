0a6ec2a051edfdbf41da355e62fb44fbeb2de6fa
Wesley Veber Pavilaki <wesley280805@edu.unipar.br>
2026-06-01 19:45:20 -0300

    deploy novo

---
+++ .github/workflows/src/components/Button.jsx
@@ -0,0 +1,13 @@
+export default function Button(props) {
+    return (
+        <button onClick={props.funcao}>
+            {props.btnText}
+        </button>
+
+        
+    )
+
+    
+
+
+}
---
+++ .github/workflows/src/components/Title.jsx
@@ -0,0 +1,7 @@
+export default function Title({titulo, subtitulo}){
+   return <div>
+     <h1>{titulo}</h1>
+     <h2>{subtitulo}</h2>
+
+    </div>
+}
